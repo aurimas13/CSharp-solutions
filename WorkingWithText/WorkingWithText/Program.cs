434ca90d5d88c882317212a49d75b6b59b9e4675
aurimas13 <aurimas.nausedas@gmail.com>
2021-08-08 13:22:06 +0300

    Add files via upload

---
+++ WorkingWithText/WorkingWithText/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Aurimas Nausedas ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim()); // Trim: 'Aurimas Nausedas'
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper()); // ToUpper: 'AURIMAS NAUSEDAS'
+            // Strings are immutable
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName); // First Name: Aurimas
+            Console.WriteLine("Last Name: " + lastName); // Last Name: Nausedas 
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]); // First Name: Aurimas
+            Console.WriteLine("First Name: " + names[1]); // First Name: Nausedas
+
+            Console.WriteLine(fullName.Replace("Aurimas", "Auris")); // Auris Nausedas 
+
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid"); // Invalid
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age); // 25
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C")); // $29.95
+            Console.WriteLine(price.ToString("C0")); // $30
+
+            var sentence = "This is a sentence of something new and old.";
+            var summary = StringUtility.SummarizeText(sentence);
+            Console.WriteLine(summary, 25);
+        }
+    }
+}
---
+++ WorkingWithText/WorkingWithText/StringUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithText
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(String text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+
+            return String.Join(" ", summaryWords) + " ...";
+        }
+    }
+}
