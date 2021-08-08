0eb19d28a4f4a97db8afe55f2893a94dfdc93f44
aurimas13 <aurimas.nausedas@gmail.com>
2021-08-08 13:20:10 +0300

    Initial upload

---
+++ CSharpAdditionalFundamentals/CSharpAdditionalFundamentals/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpAdditionalFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+            var numbersTwo = new List<int>() { 1, 2, 3, 4 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length); // Length: 6
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index); // Index of 9: 2
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+            foreach (var n in numbers)
+                Console.WriteLine(n); // 0 0 9 2 14 6
+
+            // Copy
+            var another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var num in another)
+                Console.WriteLine(num); // 0 0 9
+
+            // Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var num in numbers)
+                Console.WriteLine(num); // 0 0 2 6 9 14
+
+            // Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n); // 14 9 6 2 0 0
+
+            numbersTwo.Add(1);
+            numbersTwo.AddRange(new int[3] { 5, 6, 7 }); // enumerable - array or a list
+
+            foreach (var number in numbersTwo)
+                Console.WriteLine(number); // 1 2 3 4 1 5 6 7 
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: " + numbersTwo.IndexOf(1)); // Index of 1: 0
+            Console.WriteLine("Last Index of 1: " + numbersTwo.LastIndexOf(1)); // Last Index of 1: 4
+
+            Console.WriteLine("Count: " + numbersTwo.Count); // Count: 8
+
+            for (var i = 0; i < numbersTwo.Count; i++)
+            {
+                if (numbersTwo[i] == 1)
+                    numbersTwo.Remove(numbersTwo[i]);
+            }
+            foreach (var number in numbersTwo)
+                Console.WriteLine(number); // 2 3 4 5 6 7 
+
+            numbersTwo.Clear();
+            Console.WriteLine("Count: " + numbersTwo.Count); // Count: 0
+        }
+    }
+}
---
+++ Debug/Debug/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number in the list.");
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
